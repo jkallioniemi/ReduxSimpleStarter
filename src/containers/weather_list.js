d00e518e72786c43b2d7c8cd7c97d0e9501bf632
Juuso Kallioniemi <juuso.kallioniemi@vincit.fi>
2018-05-26 10:31:59 +0300

    Display chart of temperature in weather list

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
-        if (!cityData) {
-            return (
-                <tr key={cityData.city.name}>
-                    <td>{cityData.city.name}</td>
-                </tr>
-            );
-        }
+        const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
+        console.log(temps);
+
+        return (
+            <tr key={name}>
+                <td>{name}</td>
+                <td>
+                    <Sparklines height={120} width={180} data={temps}>
+                        <SparklinesLine color="red" />
+                    </Sparklines>
+                </td>
+            </tr>
+        );
     }
 
     render() {
