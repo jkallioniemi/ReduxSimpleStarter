a6b17651f708c0e118741f086ea019fed5e95428
Juuso Kallioniemi <jkallioniemi@gmail.com>
2018-05-26 11:41:49 +0300

    Display a Google map for each city

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
         const temps = cityData.list.map(weather => weather.main.temp);
         const pressures = cityData.list.map(weather => weather.main.pressure);
-        console.log(pressures);
         const humidities = cityData.list.map(weather => weather.main.humidity);
-        console.log(humidities);
+        const { lon, lat } = cityData.city.coord;
 
         return (
             <tr key={name}>
-                <td>{name}</td>
+                <td><GoogleMap lon={lon} lat={lat} /></td>
                 <td><Chart data={temps} color="red" units="K" /></td>
                 <td><Chart data={pressures} color="green" units="hPa" /></td>
                 <td><Chart data={humidities} color="blue" units="%" /></td>
