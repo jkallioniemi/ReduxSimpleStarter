b785c86ca238b4c4813dba9d4802a5140fb6f71b
Juuso Kallioniemi <jkallioniemi@gmail.com>
2018-05-27 22:37:31 +0300

    Add validation to post submission

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -4,20 +4,29 @@ import { Field, reduxForm } from "redux-form";
 class PostsNew extends Component {
     renderField(field) {
         return (
-            <div className="form-group">
+            <div className={`form-group ${field.meta.invalid && field.meta.touched ? "has-danger" : ""}`}>
                 <label>{field.label}</label>
                 <input
                     className="form-control"
                     type="text"
                     {...field.input}
                 />
+                <div className="text-help">
+                {field.meta.touched ? field.meta.error : ""}
+                </div>
             </div>
         );
     }
 
+    onSubmit(values) {
+        console.log(values);
+    }
+
     render() {
+        const { handleSubmit } = this.props;
+
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field
                     label="Title"
                     name="title"
@@ -33,11 +42,31 @@ class PostsNew extends Component {
                     name="content"
                     component={this.renderField}
                 />
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         );
     }
 }
 
+function validate(values) {
+    const errors = {};
+
+    if (!values.title) {
+        errors.title = "Enter a title!";
+    }
+
+    if (!values.tags) {
+        errors.tags = "Enter at least one tag!";
+    }
+
+    if (!values.content) {
+        errors.content = "Enter some content!";
+    }
+
+    return errors;
+}
+
 export default reduxForm({
-    form: "PostsNewForm"
+    form: "PostsNewForm",
+    validate
 })(PostsNew);
