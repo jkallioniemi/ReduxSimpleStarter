db11d9776eeb84a65e956dd55f01447c11be6a63
Juuso Kallioniemi <jkallioniemi@gmail.com>
2018-05-27 21:52:51 +0300

    Add new post page and navigation to said page

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from "../actions";
+import { Link } from "react-router-dom";
 import _ from 'lodash';
 
 class PostsIndex extends Component {
@@ -9,10 +10,7 @@ class PostsIndex extends Component {
     }
 
     renderPosts() {
-        console.log('attempting to render posts');
-        console.log('posts are: ', this.props.posts);
         return _.map(this.props.posts, post => {
-            console.log('rendering post', post);
             return (
                 <li className="list-group-item" key={post.id}>
                     {post.title}
@@ -24,6 +22,11 @@ class PostsIndex extends Component {
     render() {
         return (
             <div>
+                <div className="text-xs-right">
+                    <Link className="btn btn-primary" to="/posts/new">
+                        Add a Post
+                    </Link>
+                </div>
                 <h3>Posts</h3>
                 <ul className="list-group">
                     {this.renderPosts()}
